953a1bfd290bfa95c5c9bf34f13b6baac4625f7c
ahow_2 <ahow6552@gmail.com>
2024-05-17 23:34:18 +0800

    delete redund

--- DumpData/attend.py
+++ DumpData/attend.py
@@ -1,15 +1,9 @@
-import csv
 import sqlite3
 
 conn = sqlite3.connect('nbaDB.db')
 cursor = conn.cursor()
 source = sqlite3.connect("nba.sqlite")
 sourceCursor = source.cursor()
-
-# 讀取CSV檔案並進行欄位名稱對應
-with open('csv/line_score.csv', 'r', encoding='utf-8') as file:
-    reader = csv.DictReader(file)
-    data = [(i['game_id'], i['team_id_home'], i['team_id_away'], i['pts_home'], i['pts_away']) for i in reader]
 
 sourceCursor.execute("SELECT line_score.game_id, line_score.team_id_home, line_score.team_id_away, line_score.pts_home, line_score.pts_away from line_score join team on (line_score.team_id_home = team.id or line_score.team_id_away = team.id) join game on line_score.game_id = game.game_id;")
 data_to_import = sourceCursor.fetchall()
