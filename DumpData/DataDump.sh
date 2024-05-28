ceca40d2c1e37a01f3f2ce7b291430516fd90af2
ahow_2 <ahow6552@gmail.com>
2024-05-29 00:15:36 +0800

    add pipeline

---
+++ DumpData/DataDump.sh
@@ -0,0 +1,21 @@
+python team.py  
+echo "complete team"
+
+python season.py
+echo "complete season"
+
+python player.py
+echo "complete player"
+
+python game.py
+echo "complete game"
+
+python attend.py
+echo "complete attend"
+
+python rebound.py
+python hitAssist.py
+python steal.py
+python score.py
+python freethrow.py
+echo "complete gamerecord"
